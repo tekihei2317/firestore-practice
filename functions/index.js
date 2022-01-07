afa051ddc67212bda87ccd36f954867a04cf63ec
tekihei2317 <tekihei2317@gmail.com>
2022-01-07 09:55:54 +0900

    feat: Firebaseのエミュレータを導入

---
+++ functions/index.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+  const original = req.query.text;
+  const messageRef = await admin.firestore().collection('messages').add({ original });
+
+  res.json({ result: `Message withID: ${messageRef.id} added.` });
+});
+
+exports.makeUppercase = functions.firestore.document('/messages/{documentId}').onCreate((snap, context) => {
+  const original = snap.data().original;
+
+  functions.logger.log('Uppercasing', context.params.documentId, original);
+
+  const uppercase = original.toUpperCase();
+
+  return snap.ref.set({ uppercase }, { merge: true });
+});
